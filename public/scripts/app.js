c88d6d3c69955d026fa95ee817ba4138439e37d0
Agi Wunderlich <agi_wunderlich@yahoo.de>
2023-06-18 19:38:58 +0200

    added delete and add todo methods

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -8,32 +8,204 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-// creating a new Component and setting up state
-// VisibilityToggle Component - render, constructor, set the default state, handleToggleVisibility
+// App Component
+var App = function (_React$Component) {
+  _inherits(App, _React$Component);
 
-var VisibilityToggle = function (_React$Component) {
-  _inherits(VisibilityToggle, _React$Component);
+  function App(props) {
+    _classCallCheck(this, App);
 
-  function VisibilityToggle(props) {
-    _classCallCheck(this, VisibilityToggle);
+    var _this = _possibleConstructorReturn(this, (App.__proto__ || Object.getPrototypeOf(App)).call(this, props));
 
-    var _this = _possibleConstructorReturn(this, (VisibilityToggle.__proto__ || Object.getPrototypeOf(VisibilityToggle)).call(this, props));
-
-    _this.handleToggle = _this.handleToggle.bind(_this);
+    _this.handleDelete = _this.handleDelete.bind(_this);
+    _this.handlePick = _this.handlePick.bind(_this);
+    _this.handleAdd = _this.handleAdd.bind(_this);
     _this.state = {
-      visibility: false
+      todos: []
+      //todos: [],
     };
     return _this;
   }
 
-  _createClass(VisibilityToggle, [{
-    key: "handleToggle",
-    value: function handleToggle() {
+  _createClass(App, [{
+    key: "handleDelete",
+    value: function handleDelete(todo) {
       this.setState(function (prevState) {
         return {
-          visibility: !prevState.visibility
+          todos: prevState.todos.filter(function (item) {
+            return item !== todo;
+          })
         };
       });
+    }
+  }, {
+    key: "handlePick",
+    value: function handlePick(todo) {
+      alert(todo);
+    }
+  }, {
+    key: "handleAdd",
+    value: function handleAdd(todo) {
+      if (!todo) {
+        return "Please write something here if you want to add a task.";
+      } else if (this.state.todos.indexOf(todo) > -1) {
+        return "This task is already on your list.";
+      }
+      this.setState(function (prevState) {
+        return {
+          // concat doesn't influence the initial array like push does
+          todos: prevState.todos.concat(todo)
+        };
+      });
+    }
+  }, {
+    key: "render",
+    value: function render() {
+      var title = "TODO lista";
+      return React.createElement(
+        "div",
+        null,
+        React.createElement(List, {
+          title: title,
+          todos: this.state.todos,
+          hasOptions: this.state.todos.length > 0,
+          handleDelete: this.handleDelete,
+          handlePick: this.handlePick
+        }),
+        React.createElement(AddTodo, { handleAdd: this.handleAdd })
+      );
+    }
+  }]);
+
+  return App;
+}(React.Component);
+
+// TodoList
+
+
+var List = function (_React$Component2) {
+  _inherits(List, _React$Component2);
+
+  function List() {
+    _classCallCheck(this, List);
+
+    return _possibleConstructorReturn(this, (List.__proto__ || Object.getPrototypeOf(List)).apply(this, arguments));
+  }
+
+  _createClass(List, [{
+    key: "render",
+    value: function render() {
+      var _this3 = this;
+
+      return React.createElement(
+        "div",
+        null,
+        React.createElement(
+          "h1",
+          null,
+          this.props.title
+        ),
+        this.props.hasOptions ? React.createElement(
+          "p",
+          null,
+          "You have the following ",
+          this.props.todos.length,
+          " tasks to complete:"
+        ) : React.createElement(
+          "p",
+          null,
+          "You can chill. There are no tasks for you today."
+        ),
+        React.createElement(
+          "ul",
+          null,
+          this.props.todos.map(function (todo) {
+            return React.createElement(Item, {
+              key: todo,
+              todoText: todo,
+              handleDelete: _this3.props.handleDelete,
+              handlePick: _this3.props.handlePick
+            });
+          })
+        )
+      );
+    }
+  }]);
+
+  return List;
+}(React.Component);
+
+var Item = function (_React$Component3) {
+  _inherits(Item, _React$Component3);
+
+  function Item() {
+    _classCallCheck(this, Item);
+
+    return _possibleConstructorReturn(this, (Item.__proto__ || Object.getPrototypeOf(Item)).apply(this, arguments));
+  }
+
+  _createClass(Item, [{
+    key: "render",
+    value: function render() {
+      var _this5 = this;
+
+      return React.createElement(
+        "div",
+        null,
+        React.createElement(
+          "li",
+          null,
+          this.props.todoText
+        ),
+        React.createElement(
+          "button",
+          { onClick: function onClick() {
+              return _this5.props.handleDelete(_this5.props.todoText);
+            } },
+          "Done"
+        ),
+        React.createElement(
+          "button",
+          { onClick: function onClick() {
+              return _this5.props.handlePick(_this5.props.todoText);
+            } },
+          "Start Pomodoro for this"
+        )
+      );
+    }
+  }]);
+
+  return Item;
+}(React.Component);
+
+var AddTodo = function (_React$Component4) {
+  _inherits(AddTodo, _React$Component4);
+
+  function AddTodo(props) {
+    _classCallCheck(this, AddTodo);
+
+    var _this6 = _possibleConstructorReturn(this, (AddTodo.__proto__ || Object.getPrototypeOf(AddTodo)).call(this, props));
+
+    _this6.handleAdd = _this6.handleAdd.bind(_this6);
+    _this6.state = {
+      error: undefined
+    };
+    return _this6;
+  }
+
+  _createClass(AddTodo, [{
+    key: "handleAdd",
+    value: function handleAdd(e) {
+      e.preventDefault();
+      var todo = e.target.elements.todo.value.trim();
+      var error = this.props.handleAdd(todo);
+
+      this.setState(function () {
+        return {
+          error: error
+        };
+      });
+      e.target.elements.todo.value = "";
     }
   }, {
     key: "render",
@@ -44,47 +216,32 @@ var VisibilityToggle = function (_React$Component) {
         React.createElement(
           "h1",
           null,
-          "Visibility Toggle"
+          React.createElement(
+            "strong",
+            null,
+            "Add Todo Component"
+          )
         ),
-        React.createElement(
-          "button",
-          { onClick: this.handleToggle },
-          " ",
-          this.state.visibility ? "Hide Details" : "Show Details",
-          " "
-        ),
-        this.state.visibility ? React.createElement(
+        this.state.error && React.createElement(
           "p",
           null,
-          "\"Here are the details\""
-        ) : undefined
+          this.state.error
+        ),
+        React.createElement(
+          "form",
+          { onSubmit: this.handleAdd },
+          React.createElement("input", { type: "text", name: "todo" }),
+          React.createElement(
+            "button",
+            null,
+            "Add"
+          )
+        )
       );
     }
   }]);
 
-  return VisibilityToggle;
+  return AddTodo;
 }(React.Component);
 
-ReactDOM.render(React.createElement(VisibilityToggle, null), document.getElementById("app"));
-
-/* let toggleVisibility = () => {
-  visibility = !visibility;
-  render();
-};
-
-const render = () => {
-  const jsx = (
-    <div>
-      <h1>Visibility Toggle</h1>
-      <button onClick={toggleVisibility}>
-        {visibility ? "Hide Details" : "Show Details"}
-      </button>
-      {visibility ? <p>"Here are the details"</p> : undefined}
-    </div>
-  );
-
-
-  ReactDOM.render(jsx, document.getElementById("app"));
-};
-
-render(); */
+ReactDOM.render(React.createElement(App, null), document.getElementById("app"));
--- src/app.js
+++ src/app.js
@@ -1,13 +1,50 @@
 // App Component
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleDelete = this.handleDelete.bind(this);
+    this.handlePick = this.handlePick.bind(this);
+    this.handleAdd = this.handleAdd.bind(this);
+    this.state = {
+      todos: [],
+      //todos: [],
+    };
+  }
+  handleDelete(todo) {
+    this.setState((prevState) => {
+      return {
+        todos: prevState.todos.filter((item) => item !== todo),
+      };
+    });
+  }
+  handlePick(todo) {
+    alert(todo);
+  }
+  handleAdd(todo) {
+    if (!todo) {
+      return "Please write something here if you want to add a task.";
+    } else if (this.state.todos.indexOf(todo) > -1) {
+      return "This task is already on your list.";
+    }
+    this.setState((prevState) => {
+      return {
+        // concat doesn't influence the initial array like push does
+        todos: prevState.todos.concat(todo),
+      };
+    });
+  }
   render() {
     const title = "TODO lista";
-
-    const todos = ["thing 1", "thing2", "thing3"];
     return (
       <div>
-        <List title={title} todos={todos} />
-        <AddTodo />
+        <List
+          title={title}
+          todos={this.state.todos}
+          hasOptions={this.state.todos.length > 0}
+          handleDelete={this.handleDelete}
+          handlePick={this.handlePick}
+        />
+        <AddTodo handleAdd={this.handleAdd} />
       </div>
     );
   }
@@ -19,9 +56,21 @@ class List extends React.Component {
     return (
       <div>
         <h1>{this.props.title}</h1>
+        {this.props.hasOptions ? (
+          <p>
+            You have the following {this.props.todos.length} tasks to complete:
+          </p>
+        ) : (
+          <p>You can chill. There are no tasks for you today.</p>
+        )}
         <ul>
           {this.props.todos.map((todo) => (
-            <Item key={todo} todoText={todo} />
+            <Item
+              key={todo}
+              todoText={todo}
+              handleDelete={this.props.handleDelete}
+              handlePick={this.props.handlePick}
+            />
           ))}
         </ul>
       </div>
@@ -30,26 +79,41 @@ class List extends React.Component {
 }
 
 class Item extends React.Component {
-  handleRemove() {
-    alert("handleRemove");
-  }
   render() {
     return (
       <div>
         <li>{this.props.todoText}</li>
-        <button onClick={this.handleRemove}>Done</button>
+        {/*arrow function helps to only run the function when the button is clicked, not immediately*/}
+        <button onClick={() => this.props.handleDelete(this.props.todoText)}>
+          Done
+        </button>
+        <button onClick={() => this.props.handlePick(this.props.todoText)}>
+          Start Pomodoro for this
+        </button>
       </div>
     );
   }
 }
 
 class AddTodo extends React.Component {
-  handleAddTodo(e) {
+  constructor(props) {
+    super(props);
+    this.handleAdd = this.handleAdd.bind(this);
+    this.state = {
+      error: undefined,
+    };
+  }
+  handleAdd(e) {
     e.preventDefault();
     const todo = e.target.elements.todo.value.trim();
-    if (todo) {
-      alert(todo);
-    }
+    const error = this.props.handleAdd(todo);
+
+    this.setState(() => {
+      return {
+        error: error,
+      };
+    });
+    e.target.elements.todo.value = "";
   }
   render() {
     return (
@@ -57,7 +121,8 @@ class AddTodo extends React.Component {
         <h1>
           <strong>Add Todo Component</strong>
         </h1>
-        <form onSubmit={this.handleAddTodo}>
+        {this.state.error && <p>{this.state.error}</p>}
+        <form onSubmit={this.handleAdd}>
           <input type="text" name="todo"></input>
           <button>Add</button>
         </form>
